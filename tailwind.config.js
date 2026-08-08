c4407a3a88e6c124b67f9190d65a997c4372fb50
hermesuptma <hermesuptma@gmail.com>
2026-08-08 02:17:19 -0400

    Configurar paleta de colores personalizada para Hermes

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,23 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                hermes: {
+                    azul: {
+                        50: '#eef4f9',
+                        100: '#d3e3ee',
+                        600: '#1e3a5f',
+                        700: '#17304d',
+                        900: '#0d1b2e',
+                    },
+                    dorado: {
+                        100: '#f7ecd6',
+                        400: '#d4a24c',
+                        500: '#c08f3a',
+                        600: '#a67a2e',
+                    },
+                },
+            },
         },
     },
 
